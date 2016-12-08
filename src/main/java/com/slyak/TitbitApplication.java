b2a4c2ee09dce820a1c28d3d403db301e1142e12
stormning <stormning@163.com>
2016-12-08 19:39:33 +0800

    first commit

---
+++ src/main/java/com/slyak/TitbitApplication.java
@@ -0,0 +1,12 @@
+package com.slyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitbitApplication.class, args);
+	}
+}
