7831c0202b74a8e09274ab801c3b6bc34c64509c
zhouwei <170491295@qq.com>
2017-01-20 16:23:54 +0800

    Merge branch 'master' of /Users/zhouwei/IdeaProjects/titbit with conflicts.

--- titbit-service/src/main/java/com/slyak/TitbitApplication.java
+++ titbit-service/src/main/java/com/slyak/TitbitApplication.java
@@ -1,13 +1,16 @@
 package com.slyak;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
 @EnableJdbcHttpSession
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "com.slyak", repositoryBaseClass = GenericJpaRepositoryImpl.class, repositoryFactoryBeanClass = GenericJpaRepositoryFactoryBean.class)
+@EntityScan({"com.slyak"})
 public class TitbitApplication {
 
 	public static void main(String[] args) {
