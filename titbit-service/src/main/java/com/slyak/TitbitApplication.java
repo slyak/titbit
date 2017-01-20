4c157575024a4099b2a31d67610836d85afa2e8a
stormning <stormning@163.com>
2017-01-20 16:23:17 +0800

    remove unused

--- titbit-service/src/main/java/com/slyak/TitbitApplication.java
+++ titbit-service/src/main/java/com/slyak/TitbitApplication.java
@@ -1,17 +1,13 @@
 package com.slyak;
 
-import com.slyak.spring.jpa.GenericJpaRepositoryFactoryBean;
-import com.slyak.spring.jpa.GenericJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
 @EnableJdbcHttpSession
-@EnableJpaRepositories(basePackages = "com.slyak", repositoryBaseClass = GenericJpaRepositoryImpl.class, repositoryFactoryBeanClass = GenericJpaRepositoryFactoryBean.class)
-@EntityScan({"com.slyak"})
+@EnableJpaRepositories
 public class TitbitApplication {
 
 	public static void main(String[] args) {
